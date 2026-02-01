a5a265a6c445f8fd5516622f6408911486b09f35
syhanjin <syhanjin666@gmail.com>
2026-02-01 23:47:10 +0800

    Initial commit

---
+++ UserCode/app.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file    app.cpp
+ * @author  syhanjin
+ * @date    2026-02-01
+ */
+#include "app.hpp"
+#include "cmsis_os2.h"
+
+/**
+ * @brief Function implementing the initTask thread.
+ * @param argument: Not used
+ * @retval None
+ */
+extern "C" void Init(void* argument)
+{
+    /* 初始化代码 */
+
+    /* 初始化完成后退出线程 */
+    osThreadExit();
+}
