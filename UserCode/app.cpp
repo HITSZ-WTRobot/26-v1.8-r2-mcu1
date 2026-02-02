cb40082674c4631516c5c28cb62965cdfc430da7
syhanjin <syhanjin666@gmail.com>
2026-02-02 16:59:03 +0800

    feat: add chassis and device initialization with timer callbacks

--- UserCode/app.cpp
+++ UserCode/app.cpp
@@ -4,7 +4,29 @@
  * @date    2026-02-01
  */
 #include "app.hpp"
+
+#include "chassis.hpp"
 #include "cmsis_os2.h"
+#include "device.hpp"
+#include "dji.hpp"
+#include "system.hpp"
+#include "tim.h"
+
+size_t prescaler = 0;
+
+extern "C" void TIM_Callback_1kHz(TIM_HandleTypeDef* htim)
+{
+    APP_Chassis_Update_1kHz();
+
+    APP_Device_Update_1kHz();
+
+    service::Watchdog::EatAll();
+}
+
+extern "C" void TIM_Callback_200Hz(TIM_HandleTypeDef* htim)
+{
+    APP_Chassis_Update_200Hz();
+}
 
 /**
  * @brief Function implementing the initTask thread.
@@ -14,6 +36,29 @@
 extern "C" void Init(void* argument)
 {
     /* 初始化代码 */
+    APP_Device_Init();
+
+    APP_Chassis_BeforeUpdate();
+
+    // 启动定时器
+    HAL_TIM_RegisterCallback(&htim6, HAL_TIM_PERIOD_ELAPSED_CB_ID, TIM_Callback_1kHz);
+    HAL_TIM_Base_Start_IT(&htim6);
+    HAL_TIM_RegisterCallback(&htim13, HAL_TIM_PERIOD_ELAPSED_CB_ID, TIM_Callback_200Hz);
+    HAL_TIM_Base_Start_IT(&htim13);
+
+    // 等待各设备连接
+    APP_Device_WaitConnections();
+
+    /**
+     * 等待各种东西更新
+     */
+    osDelay(2000);
+
+    APP_Chassis_Init();
+
+    osDelay(1000);
+
+    osEventFlagsSet(systemEventHandle, SYSTEM_INITIALIZED);
 
     /* 初始化完成后退出线程 */
     osThreadExit();
